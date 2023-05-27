404b1ec1e5ef524886fb6500d944f62ce7911d57
chinobaezz <baezalbino@gmail.com>
2023-05-26 22:20:55 -0300

    25 de mayo7

--- src/main/java/com/portfoliohcc/hcc/Controller/CProyecto.java
+++ src/main/java/com/portfoliohcc/hcc/Controller/CProyecto.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyect")
-@CrossOrigin("*")//(origins = {"https://porfolio-hcc.web.app","http://localhost:4200"})
+@CrossOrigin(origins= "https://chino-frontend.web.app")
 public class CProyecto {
     
     @Autowired
